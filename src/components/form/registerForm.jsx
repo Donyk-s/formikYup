158992d83c0e832be318699820f0ab3cb3b5f4b3
Sergey Donyk <doncarlos@i.ua>
2023-08-10 01:49:55 +0300

    addFormik

--- src/components/form/registerForm.jsx
+++ src/components/form/registerForm.jsx
@@ -1,18 +1,30 @@
-export const RegisterForm = () => {
-  return (
-    <form autoComplete="off">
-      <label htmlFor="name">
-        {' '}
-        <input type="text" name="name" />
-      </label>
-      <label htmlFor="email">
-        <input type="email" name="email" />
-      </label>
-      <label htmlFor="password">
-        <input type="text" name="password" />
-      </label>
+import { Formik, Form, Field } from 'formik';
 
-      <button type="submit">Submit</button>
-    </form>
+const initialValues = {
+  name: '1',
+  email: 'F',
+  password: 'Q',
+};
+export const RegisterForm = () => {
+  const handleSubmit = (values, actions) => {
+    console.log(values);
+    console.log(actions);
+  };
+  return (
+    <Formik initialValues={initialValues} onSubmit={handleSubmit}>
+      <Form autoComplete="off">
+        <label htmlFor="name">
+          Name: <Field type="text" name="name" />
+        </label>
+        <label htmlFor="email">
+          Email: <Field type="email" name="email" />
+        </label>
+        <label htmlFor="password">
+          Password: <Field type="text" name="password" />
+        </label>
+
+        <button type="submit">Submit</button>
+      </Form>
+    </Formik>
   );
 };
